8b05ed742d5e44a64fbb2c1b56e49e78c5e933f2
polla <polla@polla-Lenovo-B460.(none)>
2014-04-28 16:42:37 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '166399848a8ac1e0824b140818d42578c92a73d9ba0d843037c46c79f569e6bdb65af881eb7e1b194e9437cc0950063afdf839c565619345fe2e0866ae168623'
